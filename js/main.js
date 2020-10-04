9542b4db74b8c11bd4e9bfed7e2f5deaad5f30b9
NotThatDavidCross <dcross1@gmail.com>
2020-10-04 11:00:46 -0500

    Add IDs to the table

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ function find_lineups(player_list) {
             lineup_salary = player_list[i]["Salary"] + player_list[j]["Salary"] + player_list[k]["Salary"] + player_list[l]["Salary"] + player_list[m]["Salary"];
             lineup_points = player_list[i]["FPPG"] + player_list[j]["FPPG"] + player_list[k]["FPPG"] + player_list[l]["FPPG"] + player_list[m]["FPPG"];
             if (lineup_salary <= 60000 && lineup_salary >= min_salary && lineup_points >= min_points) {
-              lineup_list.push(lineup_hash(lineup_points, lineup_salary, player_list[i]["Nickname"], player_list[j]["Nickname"], player_list[k]["Nickname"], player_list[l]["Nickname"], player_list[m]["Nickname"]));
+              lineup_list.push(lineup_hash(lineup_points, lineup_salary, player_list[i]["Nickname"], player_list[i]["Id"], player_list[j]["Nickname"], player_list[j]["Id"], player_list[k]["Nickname"], player_list[k]["Id"], player_list[l]["Nickname"], player_list[l]["Id"], player_list[m]["Nickname"], player_list[m]["Id"]));
             }
           }
         }
@@ -68,30 +68,45 @@ function display_data(lineup_list) {
     let cell_points = row.insertCell(0);
     let cell_salary = row.insertCell(1);
     let cell_player1 = row.insertCell(2);
-    let cell_player2 = row.insertCell(3);
-    let cell_player3 = row.insertCell(4);
-    let cell_player4 = row.insertCell(5);
-    let cell_player5 = row.insertCell(6);
+    let cell_player1_id = row.insertCell(3);
+    let cell_player2 = row.insertCell(4);
+    let cell_player2_id = row.insertCell(5);
+    let cell_player3 = row.insertCell(6);
+    let cell_player3_id = row.insertCell(7);
+    let cell_player4 = row.insertCell(8);
+    let cell_player4_id = row.insertCell(9);
+    let cell_player5 = row.insertCell(10);
+    let cell_player5_id = row.insertCell(11);
 
     cell_points.innerHTML = lineup_list[i]["points"].toFixed(2);
     cell_salary.innerHTML = "$" + lineup_list[i]["salary"];
     cell_player1.innerHTML = lineup_list[i]["player1"];
+    cell_player1_id.innerHTML = lineup_list[i]["player1_id"];
     cell_player2.innerHTML = lineup_list[i]["player2"];
+    cell_player2_id.innerHTML = lineup_list[i]["player2_id"];
     cell_player3.innerHTML = lineup_list[i]["player3"];
+    cell_player3_id.innerHTML = lineup_list[i]["player3_id"];
     cell_player4.innerHTML = lineup_list[i]["player4"];
+    cell_player4_id.innerHTML = lineup_list[i]["player4_id"];
     cell_player5.innerHTML = lineup_list[i]["player5"];
+    cell_player5_id.innerHTML = lineup_list[i]["player5_id"];
   }
 }
 
-function lineup_hash(points, salary, player1, player2, player3, player4, player5) {
+function lineup_hash(points, salary, player1, player1_id, player2, player2_id, player3, player3_id, player4, player4_id, player5, player5_id) {
   var l = {};
   l["points"] = points;
   l["salary"] = salary;
   l["player1"] = player1;
+  l["player1_id"] = player1_id;
   l["player2"] = player2;
+  l["player2_id"] = player2_id;
   l["player3"] = player3;
+  l["player3_id"] = player3_id;
   l["player4"] = player4;
+  l["player4_id"] = player4_id;
   l["player5"] = player5;
+  l["player5_id"] = player5_id;
   return l;
 }
 
